0aaab2139bc9a35f9261c2c230a3c97a457078c7
Mvsangh <20cs074@gmail.com>
2022-02-28 19:52:29 +0530

    Completed Solution of PECEX1

--- Assignment Problems/Assignment-4/PECEX1C.cpp
+++ Assignment Problems/Assignment-4/PECEX1C.cpp
@@ -2,54 +2,72 @@
 using namespace std;
 #define rep(i, size) for (int i = 0; i < size; i++)
 
-bool isPermu(long long int arr1[],long long int arr2[],int l,int r)
+bool isPermu(long long int arr1[], long long int arr2[], int l, int r)
 {
-        int n=r-l+1;
-        sort(arr1,arr1+n);
-        sort(arr2,arr2+n);
+        int n = r - l + 1;
+        // sort(arr1,arr1+n);
+        // sort(arr2,arr2+n);
 
-        for (int i = 0; i <n; i++)
+        // making a copy of both arrays is important because after sorting the result might be afected in
+        // some test cases
+
+        long long int copy1[n], copy2[n];
+
+        // rep(i,n) copy1[i]=arr1[i+l];
+        // rep(i,n) copy2[i]=arr2[i+l];
+
+        for (int i = 0; i < n; i++)
         {
-                if(arr1[i]!=arr2[i])
+                copy1[i] = arr1[i + l];
+                copy2[i] = arr2[i + l];
+        }
+        // now we can sort the copied arrays inorder to get thre required results
+        sort(copy1, copy1 + n);
+        sort(copy2, copy2 + n);
+
+        for (int i = 0; i < n; i++)
+        {
+                if (copy1[i] != copy2[i])
                 {
                         return false;
                 }
         }
-        
-        return true;
 
+        return true;
 }
 int main()
 {
         int t;
-        cin>>t;
+        cin >> t;
         while (t--)
         {
                 int n;
-                cin>>n;
-                long long int a[n],b[n];     
-                rep(i,n)cin>>a[i];
-                rep(i,n)cin>>b[i];
+                cin >> n;
+                long long int a[n], b[n];
+                rep(i, n) cin >> a[i];
+                rep(i, n) cin >> b[i];
 
-                int max=INT_MIN;
-                for (int i = 0; i <=n; i++)
+                int max = INT_MIN;
+                for (int i = 0; i < n; i++)
                 {
-                        for (int j =i; j <n; j++)
+                        for (int j = i; j < n; j++)
                         {
-                                if(isPermu(a,b,i,j))
+                                if (isPermu(a, b, i, j))
                                 {
-                                        if(max<(j-i+1))
+                                        if (max < (j - i + 1))
                                         {
-                                                max=j-i+1;
+                                                max = j - i + 1;
                                         }
                                 }
-                                
                         }
-                        
                 }
-                cout<<max<<endl;
-                
+                if (max == INT_MIN)
+                {
+                        cout << 0 << endl;
+                }
+                else
+                        cout << max << endl;
         }
-        
+
         return 0;
 }
